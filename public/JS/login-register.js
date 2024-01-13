f06f94a7265b72d0ea09d46bf7cca73a561455bb
abbi964 <abhinavthapliyal964@gmail.com>
2024-01-13 19:36:43 +0530

    Adding Login frontend

--- public/JS/login-register.js
+++ public/JS/login-register.js
@@ -7,10 +7,12 @@ const signupEmail = document.getElementById('signup-email');
 const signupPassword = document.getElementById('signup-password');
 const signupPassConfrm = document.getElementById('signup-passConfrm');
 const signupSubmitBtn = document.getElementById('signup-submit');
+const signupAlert = document.getElementById('signup-alert');
 
 const signinEmail = document.getElementById('signin-email');
 const signinPassword = document.getElementById('signin-password');
 const signinSubmitBtn = document.getElementById('signin-submit');
+const signinAlert = document.getElementById('signin-alert');
 
 
 // For overalys
@@ -23,15 +25,17 @@ signInButton.addEventListener('click', () => {
 });
 
 
-//--------------------- Handling Register ----------------------------------------//
+//--------------------- Handling Signup ----------------------------------------//
 signupSubmitBtn.addEventListener('click',async(e)=>{
 	e.preventDefault()
 	try{
 		if (signupName.value === '' || signupEmail.value === '' || signupPassword.value === '' || signupPassConfrm.value === ''){
-			window.alert('Please fill all the fields')
+			signupAlert.innerHTML = 'Please fill all the fields'
+			setTimeout(()=>signupAlert.innerHTML = '',1000)
 		}
 		else if (signupPassword.value !== signupPassConfrm.value) {
-			window.alert("Password do not match")
+			signupAlert.innerHTML = "Password do not match"
+			setTimeout(()=>signupAlert.innerHTML = '',1000)
 		}
 		else{
 			// making user obj
@@ -51,14 +55,50 @@ signupSubmitBtn.addEventListener('click',async(e)=>{
 				signupPassword.value = '',
 				signupPassConfrm.value = '',
 
-				window.alert('Signup successful')
+				signupAlert.innerHTML = 'Signup successful'
+				setTimeout(()=>signupAlert.innerHTML = '',1000)
 			}
 			else{
-				window.alert('Something Went wrong...')
+				signupAlert.innerHTML = 'Something Went wrong...'
+				setTimeout(()=>signupAlert.innerHTML = '',1000)
 			}
 		}
 	}
 	catch(err){
 		console.log(err);
+	}
+})
+
+
+//------------------------- Handling Sign In ----------------------------------//
+signinSubmitBtn.addEventListener('click',async(e)=>{
+	e.preventDefault();
+	try{
+		if (signinEmail.value === '' || signinPassword.value === ''){
+			signinAlert.innerHTML = 'Please fill all the fields'
+			setTimeout(()=>signinAlert.innerHTML = '',1000)
+		}
+		else{
+			// making a login req
+			let obj = {
+				email : signinEmail.value,
+				password : signinPassword.value
+			}
+
+			let response = await axios.post("http://localhost:3000/auth/login",obj);
+
+			if (response.data.user){
+				// if login is successfull then storing the accesskey in localstorage
+				localStorage.setItem('token',response.data.accessToken)
+	
+				// redirecting to home page
+				window.location.href = '/movie/home'
+			}
+		}
+	}
+	catch(err){
+		signinAlert.innerHTML = err.response.data.msg
+		setTimeout(()=>signinAlert.innerHTML = '',1000)
+		console.log(err)
 	}
 })
